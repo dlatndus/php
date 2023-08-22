cd0d4ba7e1b8a401fdfa1014f32336386d0924ea
임수연 <81746616+dlatndus@users.noreply.github.com>
2023-08-23 01:47:03 +0900

    php 연산자

---
+++ 8-operator.php
@@ -0,0 +1,18 @@
+<?php
+$sum = 5+5;
+
+$minus = 5-2;
+
+$mul = 5*3;
+
+$division = 6/3;
+
+$rest = 5%3;
+
+echo "더하기".$sum."<br>".
+"빼기".$minus."<br>".
+"곱하기".$mul."<br>".
+"나누기".$division."<br>".
+"나머지".$rest."<br>"
+
+?>
