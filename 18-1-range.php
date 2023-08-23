28c2194bcb7f5466da972105f004cd97e878170a
임수연 <81746616+dlatndus@users.noreply.github.com>
2023-08-24 00:17:11 +0900

    php range()

---
+++ 18-1-range.php
@@ -0,0 +1,14 @@
+<?php
+//1부터 10까지의 배열
+//index 0에 1 1에 2 ...
+$num = range(1,10);
+
+//echo $num[0];
+
+echo "<pre>";
+var_dump($num);
+echo "</pre>";
+
+?>
+
+
---
+++ 18-2-range.php
@@ -0,0 +1,13 @@
+<?php
+//3번째 매개변수는 간격
+$num = range(1,10, 3);
+
+//echo $num[0];
+
+echo "<pre>";
+var_dump($num);
+echo "</pre>";
+
+?>
+
+
