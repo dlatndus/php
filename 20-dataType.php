f442da33abf8abc0ae883f5d111af32db909a1cb
임수연 <81746616+dlatndus@users.noreply.github.com>
2023-08-24 00:38:53 +0900

    php gettype()

---
+++ 20-dataType.php
@@ -0,0 +1,28 @@
+<?php
+$num = 12;
+echo "\$num 의 타입은 ".gettype($num);
+echo "<br>";
+
+$greeting = "하이";
+echo "\$greeting 의 타입은 ".gettype($greeting);
+echo "<br>";
+
+$numStr = "123";
+echo "\$numStr 의 타입은 ".gettype($numStr);
+echo "<br>";
+
+$fruit = array();
+echo "\$fruit 의 타입은 ".gettype($fruit);
+echo "<br>";
+
+$nai = null;
+echo "\$nai 의 타입은 ".gettype($nai);
+echo "<br>";
+
+$boolean = true;
+echo "\$boolean 의 타입은 ".gettype($boolean);
+echo "<br>";
+
+?>
+
+
