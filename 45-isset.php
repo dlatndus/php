9465ea1aa5d9127a1280b26a64e13add5d2552eb
임수연 <81746616+dlatndus@users.noreply.github.com>
2023-08-30 00:31:08 +0900

    php isset

---
+++ 45-isset.php
@@ -0,0 +1,23 @@
+<?php
+
+if(isset($str)){
+    echo "str 존재";
+}
+else{
+    echo "str 없음";
+}
+
+echo "<br>";
+
+$str = "string";
+
+if(isset($str)){
+    echo "str 존재";
+}
+else{
+    echo "str 없음";
+}
+
+?>
+
+
