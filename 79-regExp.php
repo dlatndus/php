f4f0003da3f22b70e05c38f99c819c32606e922f
임수연 <81746616+dlatndus@users.noreply.github.com>
2023-09-12 01:23:48 +0900

    수정

--- 79-regExp.php
+++ 79-regExp.php
@@ -1,7 +1,7 @@
 <?php
-$pattern = '/^(010|011|016|017|018|019)-[^0][0-9]{3,4}-[0-9]{4}$/'; //휴대전화 검사 1byte이상
+$pattern = '/^[a-zA-Z]{1}[a-zA-z0-9.\-_]+@[a-z0-9]{1}(([a-z]{1}[a-z.]+[a-z]{1})|([a-z]+))$/'; //이메일 검사 1byte이상
 
-$str = '010-1234-1230';
+$str = 'asd_f@naver.com';
 
 if(preg_match($pattern, $str, $matches)){
     echo "값 {$str}은 정규식 표현에 적합한 값입니다.";
