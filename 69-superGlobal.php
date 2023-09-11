7bb8448985ae28040656c65216cd9bd84ae3394a
임수연 <81746616+dlatndus@users.noreply.github.com>
2023-09-12 00:40:19 +0900

    php server 변수

---
+++ 69-superGlobal.php
@@ -0,0 +1,12 @@
+<?php
+echo 'DOCUMENT_ROOT is '.$_SERVER['DOCUMENT_ROOT'].'<br>'; 
+echo 'HTTP_ACCEPT_LANGUAGE is '.$_SERVER['HTTP_ACCEPT_LANGUAGE'] . '<br>';
+echo 'HTTP HOST is '.$_SERVER['HTTP_HOST'].'<br>';
+echo 'HTTP USER_AGENT is '.$_SERVER['HTTP_USER_AGENT'].'<br>';
+echo 'SERVER PORT is '.$_SERVER['SERVER_PORT'].'<br>';
+echo 'SCRIPT NAME is '.$_SERVER['SCRIPT_NAME'].'<br>';
+echo 'REQUEST URI is '.$_SERVER['REQUEST_URI'].'<br>';
+echo 'PHP SELF is '.$_SERVER['PHP_SELF'].'<br>'; 
+echo 'QUERY STRING is '.$_SERVER['QUERY_STRING'];
+?>
+
